3340e40b81604c52d1e62135491852c2c162b95d
jmjwork <2834623342@qq.com>
2015-05-30 02:19:20 +0800

    Update vmm.c

--- vmm.c
+++ vmm.c
@@ -1338,7 +1338,7 @@ int main(int argc, char* argv[])
 		else if(c == 'y'||c == 'Y')
 			do_print_info();
 		else if(c == 'a'||c == 'A')
-			do_print_zctMem();
+			do_print_actual();
 		else if(c == 'n'||c =='N')
 		{
 			ptr_memAccReq = &(cmd.request);
